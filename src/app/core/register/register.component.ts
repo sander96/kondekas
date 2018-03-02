cbc1064f9b6c426c09942faa1d74bf78451d2d23
HermanMeier <herman.meier@hotmail.com>
2018-03-02 14:35:04 +0200

    Added requirements for registration form

--- src/app/core/register/register.component.ts
+++ src/app/core/register/register.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   moduleId: module.id,
   templateUrl: './register.component.html'
 })
-export class RegisterComponent { }
+export class RegisterComponent { 
+}
