091954768c415edcbd9b348f9facc33d624ae624
sander96 <sanderparn96@gmail.com>
2018-03-15 19:28:57 +0200

    Changed email to gmail

--- server/controllers/email.js
+++ server/controllers/email.js
@@ -1,15 +1,23 @@
-module.exports.sendEmail = function (address, name) {
-  var mailgun = require('mailgun-js')({
-    apiKey: process.env.MAILGUN_API_KEY,
-    domain: process.env.MAILGUN_DOMAIN
-  });
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
 
-  var data = {
-    from: 'Kondekas <kondekas@mailgun.org>',
+var transporter = nodemailer.createTransport(smtpTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD
+  }
+}));
+
+
+module.exports.sendEmail = function (address, name) {
+  var mailOptions = {
+    from: process.env.EMAIL,
     to: address,
     subject: 'Kondekas - Web Application Development course',
     text: 'Hello ' + name + ', this is an automated message from kondekas.'
   };
 
-  mailgun.messages().send(data);
+  transporter.sendMail(mailOptions);
 }
