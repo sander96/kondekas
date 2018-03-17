ad540ced0c530ada230b0149be3222a82960f529
Markus Saarniit <griips21@gmail.com>
2018-03-17 14:12:47 +0200

    Still http options

--- src/app/core/services/categories.service.ts
+++ src/app/core/services/categories.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable } from 'rxjs/Observable';
 import { Category } from '../models/category.model'
+import {TranslateService} from "@ngx-translate/core";
 
 @Injectable()
 export class CategoriesService {
@@ -9,9 +10,11 @@ export class CategoriesService {
   private categoriesUrl = 'api/category'
 
   constructor(
-    private http: HttpClient) { }
+    private http: HttpClient,
+    private translate: TranslateService) { }
 
   getCategories(): Observable<Category[]> {
-    return this.http.get<Category[]>(this.categoriesUrl);
+    let httpOptions = new HttpHeaders({'Accept-Language': this.translate.currentLang});
+    return this.http.get<Category[]>(this.categoriesUrl, { headers: httpOptions});
   }
 }
