57e65e55d2abda26fee17bfdaf503b8313b3e79d
Markus Saarniit <griips21@gmail.com>
2018-03-17 14:04:31 +0200

    Language header for product service

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import {HttpClient, HttpHeaders} from '@angular/common/http'
 import { Product } from '../models/product.model'
+import {TranslateService} from "@ngx-translate/core";
 
 @Injectable()
 export class ProductService {
 
   private productUrl = 'api/product/'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private translate: TranslateService) { }
 
   getProductsByPath(category: string, subCategory: string) {
     //Check for errors
+    let headers = new HttpHeaders({ 'Accept-Language': this.translate.currentLang });
     return this.http.get<Product[]>(this.productUrl + encodeURIComponent(category)
-        +'/' + encodeURIComponent(subCategory));
+        +'/' + encodeURIComponent(subCategory), { headers: headers });
   }
 }
